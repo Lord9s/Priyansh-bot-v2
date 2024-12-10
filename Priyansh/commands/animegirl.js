1a3cc56a5f0ccafbd2c1bc3fc034f592778001eb
Lord9s <nkunajunior399@gmail.com>
2024-12-10 19:44:01 +0200

    Update animegirl.js

--- Priyansh/commands/animegirl.js
+++ Priyansh/commands/animegirl.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "animegirl",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝗧𝗲𝗮𝗺 𝗦𝗺𝗶𝘁𝗵",
   description: "Random Loli Pics",
   commandCategory: "nsfw",
   usages: "lolilewd",
