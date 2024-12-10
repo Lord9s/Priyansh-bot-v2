c19269934cb7a12afc96d8ae2d44d62ddf5ed310
Lord9s <nkunajunior399@gmail.com>
2024-12-10 19:38:50 +0200

    Update alllin.js

--- Priyansh/commands/alllin.js
+++ Priyansh/commands/alllin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setallbox",
   version: "1.0.9",
 	hasPermssion: 1,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗝𝗮𝘆𝗱𝗲𝗻 𝗦𝗺𝗶𝘁𝗵",
 	description: "Change something of group",
 	commandCategory: "box",
 	usages: "set [emoji/avt/Bname/name/QTV/rcolor] [args]",
